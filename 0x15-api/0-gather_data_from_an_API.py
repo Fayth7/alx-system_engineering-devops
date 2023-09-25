9951b83bcd74e53157989ce9d193bab8bcc7b1ac
Fayth7 <flexflakes@gmail.com>
2023-09-25 16:38:45 +0300

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,36 +3,34 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-
+def get_employee_todo_progress(employee_id):
     user_info = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     ).json()
 
     if not user_info:
-        print("No employee found with ID {}".format(employee_id))
-        sys.exit(1)
+        print(f"No employee found with ID {employee_id}")
+        return
 
     employee_name = user_info.get("name")
 
     todo_list = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-            employee_id)
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     ).json()
 
     total_tasks = len(todo_list)
     completed_tasks = [task for task in todo_list if task.get("completed")]
 
     print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name, len(completed_tasks), total_tasks
-        )
+        f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):"
     )
 
     for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+        print(f"    {task.get('title')}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit
