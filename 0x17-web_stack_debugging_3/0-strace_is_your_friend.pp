4a7a2702e95cd3b274814637f2767aeb46964d66
Fayth7 <flexflakes@gmail.com>
2023-10-11 14:29:30 +0300

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+# 0-strace_is_your_friend.pp
+
+# Puppet manifest to fix Apache 500 error
+
+exec {'/etc/php5/apache2/php.ini':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini",
+}
+
+exec { '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+}
+
+exec {'/etc/init.d/apache2':
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => '/etc/init.d/apache2 restart',
+}
